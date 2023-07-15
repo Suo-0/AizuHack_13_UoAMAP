71ac18ffee6128f78962296729a8051302c4c6ed
Suo-0 <131347382+Suo-0@users.noreply.github.com>
2023-07-15 15:38:54 +0900

    色の変更と検索機能の実装

--- src/components/SearchResult.vue
+++ src/components/SearchResult.vue
@@ -22,9 +22,9 @@
 
 
 
-    <div class="q-pa-md" style="width: 720px; margin: 0 auto;">
-      <q-scroll-area style="height: 200px;">
-        <q-table
+    <div class="q-pa-sm" style="background-color: #95e7ca8b; width: 720px; margin: 0 auto;">
+      <q-scroll-area style="height: 130px;">
+        <q-table style="background-color: #cedbf236;"
         :grid="$q.screen.xs"
         flat bordered
 
@@ -33,6 +33,9 @@
         row-key="name"
         :filter="filter"
         hide-header
+        hide-pagination
+        :pagination="pagination"
+        :rows-per-page-options="[0]"
       >
       <!-- <template v-slot:top-right>
         <q-input borderless dense debounce="300" v-model="filter" placeholder="Search">
@@ -51,81 +54,477 @@
 import { ref, watch } from 'vue'
 
 const columns = [
-  {
-    name: 'desc',
+{
+    name: 'name',
     required: true,
-    label: 'Dessert (100g serving)',
+    label: '名前',
     align: 'left',
     field: row => row.name,
     format: val => `${val}`,
     sortable: true
   },
-  { name: 'calories', align: 'center', label: 'Calories', field: 'calories', sortable: true },
-  { name: 'fat', label: 'Fat (g)', field: 'fat', sortable: true },
-  { name: 'carbs', label: 'Carbs (g)', field: 'carbs' }
+  {
+    name: 'description',
+    label: '詳細',
+    align: 'left',
+    field: 'description',
+    sortable: true
+  },
+  {
+    name: 'number',
+    label: '番号',
+    align: 'center',
+    field: 'number',
+    sortable: true
+  }
 ]
 
 const rows = [
-  {
-    name: 'Frozen Yogurt',
-    calories: 159,
-    fat: 6.0,
-    carbs: 24
-  },
-  {
-    name: 'Ice cream sandwich',
-    calories: 237,
-    fat: 9.0,
-    carbs: 37
-  },
-  {
-    name: 'Eclair',
-    calories: 262,
-    fat: 16.0,
-    carbs: 23
-  },
-  {
-    name: 'Cupcake',
-    calories: 305,
-    fat: 3.7,
-    carbs: 67
-  },
-  {
-    name: 'Gingerbread',
-    calories: 356,
-    fat: 16.0,
-    carbs: 49
-  },
-  {
-    name: 'Jelly bean',
-    calories: 375,
-    fat: 0.0,
-    carbs: 94
-  },
-  {
-    name: 'Lollipop',
-    calories: 392,
-    fat: 0.2,
-    carbs: 98
-  },
-  {
-    name: 'Honeycomb',
-    calories: 408,
-    fat: 3.2,
-    carbs: 87
-  },
-  {
-    name: 'Donut',
-    calories: 452,
-    fat: 25.0,
-    carbs: 51
-  },
-  {
-    name: 'KitKat',
-    calories: 518,
-    fat: 26.0,
-    carbs: 65
-  }
+{
+        name: '研究室・実験室',
+        description: '研究棟1F',
+        number: '101'
+    },
+    {
+        name: '教員室',
+        description: '研究棟1F',
+        number: '102'
+    },
+    {
+        name: 'コンピュータ演習室(std5)',
+        description: '研究棟1F',
+        number: '103'
+    },
+    {
+        name: '研究室・実験室',
+        description: '研究棟1F',
+        number: '104'
+    },
+    {
+        name: '教員室',
+        description: '研究棟1F',
+        number: '105'
+    },
+    {
+        name: 'コンピュータ演習室(std6) ',
+        description: '研究棟1F',
+        number: '106'
+    },
+    {
+        name: 'シールド実験室',
+        description: '研究棟1F',
+        number: '107'
+    },
+    {
+        name: '電磁波シールド室',
+        description: '研究棟1F',
+        number: '108'
+    },
+    {
+        name: '音響驗室',
+        description: '研究棟1F',
+        number: '109'
+    },
+    {
+        name: '無響室',
+        description: '研究棟1F',
+        number: '110'
+    },
+    {
+        name: 'ネットワーク・遠隔講義室',
+        description: '研究棟1F',
+        number: '111'
+    },
+    {
+        name: '研究室・実験室',
+        description: '研究棟1F',
+        number: '121'
+    },
+    {
+        name: '教員室',
+        description: '研究棟1F',
+        number: '122'
+    },
+    {
+        name: '企画運営室',
+        description: '研究棟1F',
+        number: '123'
+    },
+    {
+        name: '研究棟事務連絡室',
+        description: '研究棟1F',
+        number: '124'
+    },
+    {
+        name: '研究室・実験室',
+        description: '研究棟1F',
+        number: '125'
+    },
+    {
+        name: '教員室',
+        description: '研究棟1F',
+        number: '126'
+    },
+    {
+        name: '国際戦略本部・国際交流談話室',
+        description: '研究棟1F',
+        number: '127'
+    },
+    {
+        name: '特別会議室',
+        description: '研究棟1F',
+        number: '128'
+    },
+    {
+        name: 'ゼミ室(6)-(10)',
+        description: '研究棟1F',
+        number: '129~133'
+    },
+    {
+        name: '工作室',
+        description: '研究棟1F',
+        number: '134'
+    },
+    {
+        name: '印刷室',
+        description: '研究棟1F',
+        number: '135'
+    },
+    {
+        name: '研究室・実験室',
+        description: '研究棟1F',
+        number: '141'
+    },
+    {
+        name: '教員室',
+        description: '研究棟1F',
+        number: '142'
+    },
+    {
+        name: 'コンピュータ演習室(std1)',
+        description: '研究棟1F',
+        number: '143'
+    },
+    {
+        name: '研究室・実験室',
+        description: '研究棟1F',
+        number: '144'
+    },
+    {
+        name: '教員室',
+        description: '研究棟1F',
+        number: '145'
+    },
+    {
+        name: 'コンピュータ演習室(std2)',
+        description: '研究棟1F',
+        number: '146'
+    },
+    {
+        name: '非常勤講師控室',
+        description: '研究棟1F',
+        number: '147'
+    },
+    {
+        name: '保健室',
+        description: '研究棟1F',
+        number: '148'
+    },
+    {
+        name: 'スタジオ',
+        description: '研究棟1F',
+        number: '149'
+    },
+    {
+        name: 'Geek Dojo',
+        description: '研究棟1F',
+        number: '161'
+    },
+    {
+        name: 'SRLU2',
+        description: '研究棟1F',
+        number: '162'
+    },
+    {
+        name: '研究室・実験室',
+        description: '研究棟2F',
+        number: '201'
+    },
+    {
+        name: '教員室',
+        description: '研究棟2F',
+        number: '202'
+    },
+    {
+        name: '教員室',
+        description: '研究棟2F',
+        number: '203・204'
+    },
+    {
+        name: '研究室・実験室',
+        description: '研究棟2F',
+        number: '205'
+    },
+    {
+        name: '教員室',
+        description: '研究棟2F',
+        number: '206'
+    },
+    {
+        name: '教員室',
+        description: '研究棟2F',
+        number: '207'
+    },
+    {
+        name: 'システムサポートベースSSB',
+        description: '研究棟2F',
+        number: '207-E'
+    },
+    {
+        name: '教員室',
+        description: '研究棟2F',
+        number: '207-F'
+    },
+    {
+        name: '情報センター',
+        description: '研究棟2F',
+        number: '208'
+    },
+    {
+        name: 'ハードウェア実験室（３）',
+        description: '研究棟2F',
+        number: '209'
+    },
+    {
+        name: '情報センター図書ラウンジ',
+        description: '研究棟2F',
+        number: '210'
+    },
+    {
+        name: '研究室',
+        description: '研究棟2F',
+        number: '211'
+    },
+    {
+        name: '研究室・実験室',
+        description: '研究棟2F',
+        number: '221'
+    },
+    {
+        name: '教員室',
+        description: '研究棟2F',
+        number: '222'
+    },
+    {
+        name: '研究室',
+        description: '研究棟2F',
+        number: '223'
+    },
+    {
+        name: '教員室',
+        description: '研究棟2F',
+        number: '224'
+    },
+    {
+        name: '研究室・実験室',
+        description: '研究棟2F',
+        number: '225'
+    },
+    {
+        name: '教員室',
+        description: '研究棟2F',
+        number: '226'
+    },
+    {
+        name: 'ハードウェア実験室（４）',
+        description: '研究棟2F',
+        number: '227'
+    },
+    {
+        name: '研究室・実験室',
+        description: '研究棟2F',
+        number: '241'
+    },
+    {
+        name: '教員室',
+        description: '研究棟2F',
+        number: '242'
+    },
+    {
+        name: '教員室',
+        description: '研究棟2F',
+        number: '243'
+    },
+    {
+        name: '客員教員室',
+        description: '研究棟2F',
+        number: '243-B'
+    },
+    {
+        name: '文化研究センター',
+        description: '研究棟2F',
+        number: '244・248'
+    },
+    {
+        name: '実験室',
+        description: '研究棟2F',
+        number: '245'
+    },
+    {
+        name: '修学支援室',
+        description: '研究棟2F',
+        number: '246-E'
+    },
+    {
+        name: '実験室',
+        description: '研究棟2F',
+        number: '246-F'
+    },
+    {
+        name: '教員室',
+        description: '研究棟2F',
+        number: '247'
+    },
+    {
+        name: '学生相談室',
+        description: '研究棟2F',
+        number: '252'
+    },
+    {
+        name: '語学研究室',
+        description: '研究棟2F',
+        number: '253'
+    },
+    {
+        name: 'CLR-eﾗｰﾆﾝｸﾞ･ﾕｰｻﾞﾋﾞﾘﾃｨﾗﾎﾞ',
+        description: '研究棟2F',
+        number: '261'
+    },
+    {
+        name: '語学研究室',
+        description: '研究棟2F',
+        number: '262～266・269～274'
+    },
+    {
+        name: 'CLR-技術的ｺﾐｭﾆｹｰｼｮﾝﾗﾎﾞ',
+        description: '研究棟2F',
+        number: '267'
+    },
+    {
+        name: 'CLR-音声学ラボ',
+        description: '研究棟2F',
+        number: '268'
+    },
+    {
+        name: '小講義室Ｓ１－Ｓ４',
+        description: '研究棟2F',
+        number: '275'
+    },
+    {
+        name: 'CALL1',
+        description: '研究棟2F',
+        number: '276'
+    },
+    {
+        name: 'iLab1',
+        description: '研究棟2F',
+        number: '277'
+    },
+    {
+        name: '語学研究センター事務室',
+        description: '研究棟2F',
+        number: '278'
+    },
+    {
+        name: 'iLab2',
+        description: '研究棟2F',
+        number: '279'
+    },
+    {
+        name: 'CALL2',
+        description: '研究棟2F',
+        number: '280'
+    },
+    {
+        name: '小講義室Ｓ５－Ｓ８',
+        description: '研究棟2F',
+        number: '281'
+    },
+    {
+        name: 'コンピュータ演習室(std3)',
+        description: '講義棟1F',
+        number: '101'
+    },
+    {
+        name: 'コンピュータ演習室(std4)',
+        description: '講義棟1F',
+        number: '102'
+    },
+    {
+        name: '中講義室M8',
+        description: '講義棟1F',
+        number: '103'
+    },
+    {
+        name: '中講義室M9',
+        description: '講義棟1F',
+        number: '104'
+    },
+    {
+        name: '中講義室M10',
+        description: '講義棟1F',
+        number: '105'
+    },
+    {
+        name: '中講義室M1',
+        description: '講義棟2F',
+        number: '201'
+    },
+    {
+        name: '中講義室M2',
+        description: '講義棟2F',
+        number: '202'
+    },
+    {
+        name: '中講義室M3',
+        description: '講義棟2F',
+        number: '203'
+    },
+    {
+        name: '中講義室M4',
+        description: '講義棟2F',
+        number: '204'
+    },
+    {
+        name: '中講義室M5',
+        description: '講義棟2F',
+        number: '205'
+    },
+    {
+        name: '中講義室M6',
+        description: '講義棟2F',
+        number: '206'
+    },
+    {
+        name: '中講義室M7',
+        description: '講義棟2F',
+        number: '207'
+    },
+    {
+        name: 'ハードウェア実験室（１）',
+        description: '講義棟2F',
+        number: '208'
+    },
+    {
+        name: 'ハードウェア準備室',
+        description: '講義棟2F',
+        number: '209'
+    },
+    {
+        name: 'ハードウェア実験室（２）',
+        description: '講義棟2F',
+        number: '210'
+    },
 ]
 
 export default {
@@ -145,7 +544,7 @@ export default {
 }
 </script>
 
-<style>
+<!-- <style>
 #div {
     display: flex;
     justify-content: center;
@@ -158,4 +557,4 @@ export default {
     display: flex;
     flex-direction: row;
 }
-</style>
+</style> -->
